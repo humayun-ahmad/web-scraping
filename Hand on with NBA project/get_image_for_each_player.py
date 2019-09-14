db9a79a184bf5ac628bb384c4049817689e195e9
Humayun Ahmad Rajib <humayun.ahmad.rajib@gmail.com>
2019-09-14 23:21:43 +0600

    more updating this file to taking image

--- Hand on with NBA project/get_image_for_each_player.py
+++ Hand on with NBA project/get_image_for_each_player.py
@@ -2,7 +2,8 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import os
 import requests
-
+from urllib.request import urlretrieve as p
+import random
 
 driver = webdriver.PhantomJS(executable_path = r"F:\Softwares\phantomJS\phantomjs-2.1.1-windows\phantomjs-2.1.1-windows\bin\phantomjs.exe")
 
@@ -17,11 +18,12 @@ ht_doc = soup.find('section', class_ = 'nba-player-header__item nba-player-heade
 
 img = ht_doc.find('img')
 
-
-f = open('steven_adam.jpg','wb')
-
-f.write(requests.get(img['src']).content)
+link = img['src']
+test = 'https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/203500.png'
+f = open("xxxxx.jpg", "wb")
+f.write(requests.get(test).content)
 f.close()
 driver.quit()
 
 
+#print(img['src'])

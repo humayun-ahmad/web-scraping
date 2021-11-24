ec61ac49d52c70dc927e19b5ef3b28bea30cc681
humayun-ahmad <humayunraj789@gmail.com>
2021-11-24 23:26:54 +0600

    video all data collection

--- Youtube_video_download/single_video_download_success.py
+++ Youtube_video_download/single_video_download_success.py
@@ -20,9 +20,28 @@ title = video_obj.title
 video = video_obj.streams.first()  
 video.download(save_path)   
 
+#######################################################################
+# The properties of the YouTube object are,
+# Title
+# Video Id
+# Description
+# Length
+# Thumbnail_URL
+# Views
+# Rating
+# Age Restricted
+#######################################################################
+
+
+
 print(title)
-
-
+print(video_obj.video_id)  
+print(video_obj.description)  
+print(video_obj.length)  
+print(video_obj.thumbnail_url)  
+print(video_obj.views)  
+print(video_obj.rating)  
+print(video_obj.age_restricted) 
 
 
 

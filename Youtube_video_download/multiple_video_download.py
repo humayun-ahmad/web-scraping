a17fec682ff0e3f406a67e8091a11d2a7babeabb
humayun-ahmad <humayunraj789@gmail.com>
2021-11-25 23:58:08 +0600

    add label

--- Youtube_video_download/multiple_video_download.py
+++ Youtube_video_download/multiple_video_download.py
@@ -7,4 +7,11 @@ from tkinter import *
 from pytube import YouTube
 
 
+#  Create Display Window
+root = Tk()
+root.geometry('500x300')
+root.resizable(0,0)
+root.title("youtube video downloader")
+
+Label(root,text = 'Youtube Video Downloader', font ='arial 20 bold').pack()
 
